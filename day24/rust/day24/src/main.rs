ccaf1ab6d7e48fa777f761692facb11b91167b9c
gmarait <gilles.marait@gmail.com>
2024-12-24 16:37:43 +0100

    Cleaning for part2

--- day24/rust/day24/src/main.rs
+++ day24/rust/day24/src/main.rs
@@ -80,11 +80,91 @@ fn main() {
     //println!("{:?}", states);
     //println!("{:?}", instr);
 
+    let x_wires : Vec<String> = get_wires(&states, 'x');
+    let bin_num_x = get_bin_from_wires(&states, &x_wires);
+    println!("x -> {:?}", bin_num_x);
+    let xval = bin_to_num(&bin_num_x);
+    println!("xval -> {xval}");
+
+    let y_wires : Vec<String> = get_wires(&states, 'y');
+    let bin_num_y = get_bin_from_wires(&states, &y_wires);
+    println!("y -> {:?}", bin_num_y);
+    let yval = bin_to_num(&bin_num_y);
+    println!("yval -> {xval}");
+
+    let expected_zval = xval + yval;
+    println!("expected zval -> {expected_zval}");
+    let bin_num_z = num_to_bin(expected_zval);
+    println!("expected zval binary -> {:?}", bin_num_z);
+
     part_1(&mut states, &mut instr);
 }
 
-fn part_1(states : &mut HashMap<String, bool>, instr: &Vec<Instruction>){
+fn get_wires(states : &HashMap<String, bool>, c : char) -> Vec<String>{
+    let mut c_wires : Vec<String> = Vec::new();
 
+    for (wire, _st) in states{
+        if wire.chars().nth(0) == Some(c){
+            c_wires.push(wire.to_string());
+        }
+    }
+
+    c_wires.sort();
+    return c_wires;
+}
+
+fn get_bin_from_wires(states : &HashMap<String, bool>, wires : &Vec<String>) -> Vec<char>{
+    let mut bin_num : Vec<char> = Vec::new();
+    for w in wires{
+        let val = states.get(w).unwrap();
+        if *val { bin_num.push('1'); }
+        else { bin_num.push('0'); }
+    }
+    return bin_num;
+}
+
+// Could not find a proper power function in std
+// Computes 2^exp
+fn pow_2(exp : usize) -> usize {
+    let mut res = 1;
+    for _i in 0..exp{
+        res = 2 * res;
+    }
+    return res;
+}
+
+fn bin_to_num(bin : &Vec<char>) -> usize{
+    let mut power = 0;
+    let mut num = 0;
+    for c in bin{
+        if *c == '1'{
+            num = num + pow_2(power);
+        }
+        power = power + 1;
+    }
+    return num;
+}
+
+fn num_to_bin(num : usize) -> Vec<char>{
+    let mut bin : Vec<char> = Vec::new();
+
+    let mut cur_num = num;
+    while cur_num > 1{
+        if cur_num % 2 == 1{
+            bin.push('1');
+        }
+        else{
+            bin.push('0');
+        }
+        cur_num = cur_num / 2;
+    }
+
+    bin.push('1');
+
+    return bin;
+}
+
+fn compute_circuit(states : &mut HashMap<String, bool>, instr: &Vec<Instruction>){
     let mut done = HashSet::new();
 
     while done.len() < instr.len(){
@@ -122,47 +202,19 @@ fn part_1(states : &mut HashMap<String, bool>, instr: &Vec<Instruction>){
             done.insert(i);
         }
     }
+}
 
-    println!("{:?}", states);
+fn part_1(states : &mut HashMap<String, bool>, instr: &Vec<Instruction>){
 
-    let mut z_wires : Vec<String> = Vec::new();
+    compute_circuit(states, instr);
+    //println!("{:?}", states);
 
-    for (wire, _st) in &mut *states{
-        if wire.chars().nth(0) == Some('z'){
-            z_wires.push(wire.to_string());
-        }
-    }
-
-    z_wires.sort();
-
-    println!("{:?}", z_wires);
-
-    let mut bin_num : Vec<char> = Vec::new();
-    for zw in z_wires{
-        let val = states.get(&zw).unwrap();
-        if *val { bin_num.push('1'); }
-        else { bin_num.push('0'); }
-    }
+    let z_wires : Vec<String> = get_wires(&states, 'z');
+    let bin_num = get_bin_from_wires(&states, &z_wires);
 
     println!("{:?}", bin_num);
 
-    // Could not find a proper power function in std
-    fn pow_2(exp : usize) -> usize {
-        let mut res = 1;
-        for _i in 0..exp{
-            res = 2 * res;
-        }
-        return res;
-    }
-
-    let mut power = 0;
-    let mut result = 0;
-    for c in bin_num{
-        if c == '1'{
-            result = result + pow_2(power);
-        }
-        power = power + 1;
-    }
+    let result = bin_to_num(&bin_num);
 
     println!("Result: {result}");
 }
